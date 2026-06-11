79188bb0fc3e3b441e01b7ea8b9b6a745d0b0944
Dj474 <toxap06@gmail.com>
2026-06-11 17:34:39 +0300

    fix: card entity

--- src/main/java/com/example/bankcards/entity/card/Card.java
+++ src/main/java/com/example/bankcards/entity/card/Card.java
@@ -42,6 +42,6 @@ public class Card {
     private User user;
 
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
 
 }
